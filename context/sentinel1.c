057e5ed1ff1976aa440ea116f4d33116b48bffb4
artjomPlaunov <artyemnyc@gmail.com>
2023-08-01 23:57:00 -0400

    sentinel2

--- context/sentinel1.c
+++ context/sentinel1.c
@@ -1,20 +1,12 @@
 extern void abort(void);
-
-
-extern void __assert_fail (const char *__assertion, const char *__file,
-unsigned int __line, const char *__function)
-__attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
-extern void __assert_perror_fail (int __errnum, const char *__file,
-unsigned int __line, const char *__function)
-__attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
-extern void __assert (const char *__assertion, const char *__file, int __line)
-__attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
-extern void __VERIFIER_assume(int);
-
-
-void reach_error() { ((void) sizeof ((0) ? 1 : 0), __extension__ ({ if (0) ; else __assert_fail ("0", "standard_sentinel-2.c", 3, __extension__ __PRETTY_FUNCTION__); })); }
-extern int __VERIFIER_nondet_int(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "sentinel1.c", 10, "reach_error"); }
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
+}
 void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: {reach_error();abort();} } }
+extern int __VERIFIER_nondet_int(void);
+extern void __VERIFIER_assume(int);
 int main ( ) {
 int a[100000];
 int marker = 42;
---
+++ context/sentinel2.c
@@ -0,0 +1,39 @@
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "overflow6.c", 10, "reach_error"); }
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
+}
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: {reach_error();abort();} } }
+extern int __VERIFIER_nondet_int(void);
+extern void __VERIFIER_assume(int);
+
+int main( ) {
+  int a1[2000];
+  int a2[2000];
+
+  int i;
+  int z;
+  z = 1500;
+
+  for ( i = 0 ; i < 2000 ; i++ ) {
+         a1[i] = __VERIFIER_nondet_int();
+  a2[i] = __VERIFIER_nondet_int();
+  }
+
+  for ( i = 0 ; i < 2000 ; i++ ) {
+      if (i != z)
+         a2[i] = a1[i];
+  }
+
+  int x;
+  for ( x = 0 ; x < 2000 ; x++ ) {
+      if (x != z) {
+    __VERIFIER_assume( a1[x] == a2[x]);
+    __VERIFIER_assert( a1[x] == a2[x] );
+      }
+  }
+  return 0;
+}
+
+
